aefc61ecbee6c78acef17986fb87e74dd6f84181
Mauro <maubaga92@gmail.com>
2015-09-21 22:26:43 +0200

    Add a comment

--- app/src/main/java/com/bock/tella/cucca/MainActivity.java
+++ app/src/main/java/com/bock/tella/cucca/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    // a simple method called by a button
     public void push(View view){
         TextView textView = (TextView) findViewById(R.id.text1);
         textView.setText("Hai premuto il pulsante... Bravoh!");
