4c46bace6b54c10bd5e69dc4cf77f05f88ea82bc
Mauro <maubaga92@gmail.com>
2015-09-21 16:32:18 +0200

    Second pointless commit

--- app/src/main/java/com/bock/tella/cucca/MainActivity.java
+++ app/src/main/java/com/bock/tella/cucca/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +36,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void push(View view){
+        TextView textView = (TextView) findViewById(R.id.text1);
+        textView.setText("Hai premuto il pulsante... Bravoh!");
+
+        Button button = (Button) view;
+        button.setText("Bravoh!");
     }
 }
